96ad4462815a0d206899d9268e08a4e61d38f21c
pcrglennon <pcrglennon@gmail.com>
2014-09-05 17:57:00 -0400

    Ruby Complement: Iteration 2 using a pre-defined Hash

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,17 +1,17 @@
 class Complement
 
-  DNA_NUCLEOTIDES = "ACGT"
-  RNA_NUCLEOTIDES = "UGCA"
+  DNA_COMPLEMENTS = {"A" => "U", "C" => "G", "G" => "C", "T" => "A"}
+  RNA_COMPLEMENTS = {"U" => "A", "G" => "C", "C" => "G", "A" => "T"}
 
   def self.of_dna(dna)
     dna.split('').map do |nucleotide|
-      RNA_NUCLEOTIDES[DNA_NUCLEOTIDES.index(nucleotide)]
+      DNA_COMPLEMENTS[nucleotide]
     end.join('')
   end
 
   def self.of_rna(rna)
     rna.split('').map do |nucleotide|
-      DNA_NUCLEOTIDES[RNA_NUCLEOTIDES.index(nucleotide)]
+      RNA_COMPLEMENTS[nucleotide]
     end.join('')
   end
 
